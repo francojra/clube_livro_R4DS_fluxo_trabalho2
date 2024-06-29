5ece7d0c11e29587ddd5fed3a80acecfb9bdf22e
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2024-06-28 23:46:19 -0300

    Add files via upload

--- clube_livro_R4DS.R
+++ clube_livro_R4DS.R
@@ -6,7 +6,7 @@
 
 # Capítulo 8 - Fluxo de trabalho: obtendo ajuda --------------------------------------------------------------------------------------------
 
-## Pesquisando no Google
+# Pesquisando no Google --------------------------------------------------------------------------------------------------------------------
 
 ### O Google é bom com mensagens de erro
 ### Comando para trocar idioma para inglês (pesquisar erro em inglês)
@@ -22,7 +22,7 @@ Sys.setenv(LANGUAGE = "pt")
 # Para voltar para português, fecha e abre de novo
 # ou clica em session e "Restart R"
 
-## Pesquisando no StackOverflow: https://stackoverflow.com/
+# Pesquisando no StackOverflow: https://stackoverflow.com/ ---------------------------------------------------------------------------------
 
 ### Incluir tags entre [] para especificar melhor a busca
 ### Por exemplo, se quer saber sobre o [R], ele filtra apenas R e não Python, SPSS, etc.
@@ -42,3 +42,19 @@ reprex::reprex() # Formata em Markdown para colar no StackOverflow ou GitHub
 ### Isso torna o código mais limpo, com a resposta e fácil dos leitores entenderem
 ### Valoriza o tempo das pessoas que te ajudam
 ### Outra forma rápida: seleciona o código > Addins > Reprex selection
+
+# Necessário: ------------------------------------------------------------------------------------------------------------------------------
+
+### a) Tornar reprodutível: incluir pacotes, dados e código. Também, verificar 
+### utilização da versão atualizada
+
+### Para tidyverse:
+
+library(tidyverse)
+tidyverse_update()
+
+### Outro pacotes:
+
+pacotes_antigos <- old.packages()
+
+### b) Simplificar
