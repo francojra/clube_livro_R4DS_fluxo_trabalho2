b4ca1f6abfc1a9194b05636dbea3a723d30bb835
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2024-06-28 23:11:06 -0300

    Add files via upload

--- clube_livro_R4DS.R
+++ clube_livro_R4DS.R
@@ -4,4 +4,16 @@
 # Mediadoras: Beatriz Milz e Karina Ferrara Barros -----------------------------------------------------------------------------------------
 # Data: 28/06/2024 -------------------------------------------------------------------------------------------------------------------------
 
+# Capítulo 8 - Fluxo de trabalho: obtendo ajuda --------------------------------------------------------------------------------------------
 
+## Pesquisando no Google
+## O Google é bom com mensagens de erro
+## Comando para trocar idioma para inglês (pesquisar erro em inglês)
+
+2 + x
+
+Sys.setenv(LANGUAGE = "en")
+
+2 + x
+
+Sys.setenv(LANGUAGE = "pt") # Para voltar para português, fecha e abre de novo
